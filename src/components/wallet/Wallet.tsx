2e32a501417c7d1a10e3b060f572d9ea22701a33
dev0614 <shimamuratakehiko44@gmail.com>
2022-07-06 05:25:54 +0900

    header

--- src/components/wallet/Wallet.tsx
+++ src/components/wallet/Wallet.tsx
@@ -41,7 +41,7 @@ export default function Wallet(props: { children: any }) {
 
     return (
         <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={wallets} autoConnect={true}>
+            <WalletProvider wallets={wallets} autoConnect={false}>
                 {props.children}
             </WalletProvider>
         </ConnectionProvider>
---
+++ src/pages/deploy/index.tsx
@@ -0,0 +1,11 @@
+import Header from "../../components/Header";
+import { MainPage } from "../../components/Widget";
+
+export default function DeployPage() {
+
+    return (
+        <MainPage>
+            <Header />
+        </MainPage>
+    )
+}
