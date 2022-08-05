36db83c7e77fe3009fc3bc4a616d353bc7a33ab7
dev0614 <shimamuratakehiko44@gmail.com>
2022-08-05 18:05:16 +0900

    plan item

--- src/pages/store/index.tsx
+++ src/pages/store/index.tsx
@@ -1,5 +1,6 @@
 import { NextSeo } from "next-seo";
 import Header from "../../components/Header";
+import PlanItem from "../../components/Store/PlanItem";
 import { MainPage } from "../../components/Widget";
 import { LIVE_URL } from "../../config";
 
@@ -28,66 +29,9 @@ export default function StorePage() {
             <Header back={{ title: "Store", backUrl: "/dashboard" }} />
             <MainPage>
                 <div className="store-page">
-                    <div className="plan">
-                        <div className="plan-content">
-                            <div className="plan-media">
-                                {/* eslint-disable-next-line */}
-                                <img
-                                    src="/img/store/field.jpg"
-                                    alt=""
-                                />
-                            </div>
-                            <p className="plan-label">Field Grade Loot Create</p>
-                            <p className="plan-description">Regular crate with no chance of a transcendental attribute</p>
-                            <div className="plan-ammo">
-                                <span>AMMO</span>
-                                <span>500</span>
-                            </div>
-                            <button className="btn-buy">
-                                Buy
-                            </button>
-                        </div>
-                    </div>
-                    <div className="plan">
-                        <div className="plan-content">
-                            <div className="plan-media">
-                                {/* eslint-disable-next-line */}
-                                <img
-                                    src="/img/store/company.jpg"
-                                    alt=""
-                                />
-                            </div>
-                            <p className="plan-label">Field Grade Loot Create</p>
-                            <p className="plan-description">Guaranteed universal attribute and chance of a transcendental attribute.</p>
-                            <div className="plan-ammo">
-                                <span>AMMO</span>
-                                <span>1500</span>
-                            </div>
-                            <button className="btn-buy">
-                                Buy
-                            </button>
-                        </div>
-                    </div>
-                    <div className="plan">
-                        <div className="plan-content">
-                            <div className="plan-media">
-                                {/* eslint-disable-next-line */}
-                                <img
-                                    src="/img/store/elite.jpg"
-                                    alt=""
-                                />
-                            </div>
-                            <p className="plan-label">Field Grade Loot Create</p>
-                            <p className="plan-description">Guaranteed universal attribute and chance transcendental attribute.</p>
-                            <div className="plan-ammo">
-                                <span>AMMO</span>
-                                <span>3000</span>
-                            </div>
-                            <button className="btn-buy">
-                                Buy
-                            </button>
-                        </div>
-                    </div>
+                    <PlanItem id={1} />
+                    <PlanItem id={2} />
+                    <PlanItem id={3} />
                 </div>
             </MainPage>
         </>
